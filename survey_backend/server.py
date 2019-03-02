852ba4cc769f04265f5da5ce8c0dcc6a836894d9
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2019-03-03 00:51:25 +0100

    adds persistence

--- survey_backend/server.py
+++ survey_backend/server.py
@@ -1,15 +1,23 @@
+import pathlib
+import json
 import logging
 from flask import Flask, request, abort, Response
 from flask_cors import CORS
 
+__DATABASE__ = './raw_data/'
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/submitAnswers', methods=['POST'])
 def submitAnswers():
-    if not request.json:
+    if not request.json or 'user_id' not in request.json:
         abort(400)
-    print(request.json)
+    #print(request.json)
+
+    pathlib.Path(__DATABASE__).mkdir(parents=True, exist_ok=True)
+    with open(__DATABASE__ + str(request.json['user_id']) + '.json', 'w') as f:
+        json.dump(request.json['answers'], f)
+
     return Response(status=200)
 
 @app.route('/test', methods=['GET'])
